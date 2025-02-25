c2b5b4c40104fb237378401065f9c5d1a98310c1
ameni <amenibenammar91@gmail.com>
2025-02-25 18:45:05 +0100

    premier commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const app=express()
+const CategorieRouter=require("./routes/categorie.route")
+const scategorieRouter =require("./routes/scategorie.route")
+app.use(express.json())
+
+dotenv.config()
+
+app.get('/',(req,res)=>{
+    res.send("bienvenue dans notre site")
+})
+//connection a la base de donneé
+mongoose.connect(process.env.DATABASECLOUD)
+.then(()=>{console.log("connexion a la base de doonée réussie")})
+.catch((error)=>{console.log("impossible de se connecter",error)
+process.exit()
+})
+//connecter
+app.use("/api/categories",CategorieRouter)
+app.use('/api/scategories', scategorieRouter);
+app.listen(4000,function(){
+    console.log(`serveur is listen on port ${process.env.PORT}`)
+})
+module.export = app ;
