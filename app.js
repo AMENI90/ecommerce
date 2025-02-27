9cb5278906c1a9adc85188a401ad4a3d4cf87b5c
ameni <amenibenammar91@gmail.com>
2025-02-27 22:58:27 +0100

    Commit new version

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express=require('express')
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const app=express()
+const cors=require('cors')
 const CategorieRouter=require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
 const chatbotRouter=require("./routes/chatbot.route")
 app.use(express.json())
-
+app.use(cors());
 dotenv.config()
 
 app.get('/',(req,res)=>{
@@ -21,6 +23,7 @@ process.exit()
 //connecter
 app.use("/api/chat",chatbotRouter)
 app.use("/api/categories",CategorieRouter)
+app.use("/api/article",articleRouter)
 app.use('/api/scategories', scategorieRouter);
 app.listen(4000,function(){
     console.log(`serveur is listen on port ${process.env.PORT}`)
